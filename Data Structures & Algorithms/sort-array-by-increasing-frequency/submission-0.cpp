adf6285a500a9231592559bf147caec7b90f433b
Aryan Nema <aaryan.techy@gmail.com>
2026-08-13 09:30:23 +0530

    Add: sort-array-by-increasing-frequency - submission-0

---
+++ Data Structures & Algorithms/sort-array-by-increasing-frequency/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        vector<int> ans;
+
+        for(int num : nums) {
+            freq[num]++;
+        }
+
+        sort(nums.begin(), nums.end(), [&freq](int a, int b) {
+            if(freq[a]  != freq[b]) {
+                return freq[a] < freq[b];
+            }
+
+            return a > b;
+        });
+
+        return nums;
+    }
+};
